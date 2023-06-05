6ad80480da4230d47d0e32cacba1f6a1d2408726
mayaraayat <124594237+mayaraayat@users.noreply.github.com>
2023-06-05 11:39:28 +0200

    Add files via upload

--- mae_test/categories_simulations.py
+++ mae_test/categories_simulations.py
@@ -20,7 +20,19 @@ class FMScoreWrapper(LightFM):
             return 0  # Return 0 when y is None
 
 
-def model_simulation(file_path, selected_date, category):
+def model_simulation(directory_path, selected_date, category="normal"):
+    '''
+    Parameters
+    ----------
+    directory_path (str): Path of the directory of files with the data. FMs will be outputted there.
+    selected_date (int with the month number): Investigated date
+    category (str): Category to test MAE on. Can be "cold_start", "partially_new" or "normal" (default)
+    Returns
+    -------
+    df_result (dataframe): Initial sales dataframe with the predicted score
+    '''
+   
+    print(f"Calculating MAE for {category}.\n")
 
     try:
         sales = pd.read_csv(os.path.join(
@@ -35,29 +47,24 @@ def model_simulation(file_path, selected_date, category):
         raise FileNotFoundError(
             f'The files could not be found at {directory_path}. Executing from {os.getcwd()}')
 
-        # Merge sales with stores and products
+    # Prepare datasets
     sales = sales.merge(products, on=["Material_ID"], how="left")
     sales = sales.merge(stores, on=["Store_Number"], how="left")
     df_unique_stores = stores[["Store_Number"]
                               ].drop_duplicates().reset_index(drop=True)
-
-    # Get unique Products
     df_unique_products = products[["Material_ID"]
                                   ].drop_duplicates().reset_index(drop=True)
-    # Define the cross join
     df_all_stores_all_products = df_unique_stores.merge(
         df_unique_products, how='cross')
-
-    # Add score to dataframe
     df_all_stores_all_products = df_all_stores_all_products.merge(
         sales, on=["Store_Number", "Material_ID"], how="left")
-
-    # Define the pivot matrix
     df_user_item_matrix = df_all_stores_all_products.pivot(
         index="Store_Number", columns="Material_ID", values="Defined_score")
-    print(df_user_item_matrix)
     num_unique_features = sales['Material_ID'].nunique()
+   
     print("Number of unique features:", num_unique_features)
+    
+    # Separation of categories
     if category == 'cold_start':
         if selected_date == 6:
             nbre = 500
@@ -88,8 +95,9 @@ def model_simulation(file_path, selected_date, category):
                 not_null_legit.setdefault(
                     store, []).append((i, y.loc[store][i]))
             new.loc[j, i] = np.nan
+    else:
+        new = df_user_item_matrix
 
-    # Convert the pivot matrix to a sparse matrix
     matrix = coo_matrix(new.fillna(0).values)
 
     train_data, test_data, train_row, test_row, train_col, test_col = train_test_split(
@@ -102,7 +110,6 @@ def model_simulation(file_path, selected_date, category):
     train = train_coo.tocsr()
     test = test_coo.tocsr()
 
-    # Define the parameter combinations to try
     param_combinations = [
         {'no_components': num_unique_features, 'loss': 'warp',
          'item_alpha': 0.0001, 'user_alpha': 0.0001},
@@ -112,56 +119,30 @@ def model_simulation(file_path, selected_date, category):
          'item_alpha': 0.0001, 'user_alpha': 0.0001}
     ]
 
-    # Initialize lists to store the scores and parameter combinations
     scores = []
     parameters = []
 
-    # Iterate over parameter combinations
+    # Iterate over parameter combinations for optimisation
     for params in param_combinations:
-        # Initialize the model with the current parameter combination
         model = FMScoreWrapper(**params)
-
-        # Train the model
         model.fit(train)
-
-        # Get the true ratings from the test set
         true_ratings = test.data
-
-        # Get the corresponding predicted ratings
         pred_ratings = model.predict(test_coo.row, test_coo.col)
-
-        # Clip predictions between 0 and 1
         pred_ratings = np.clip(pred_ratings, 0, 1)
-
-        # Compute the mean squared error
         score = mean_squared_error(true_ratings, pred_ratings)
-
-        # Append the score and parameters to the lists
         scores.append(score)
         parameters.append(params)
 
-        # Find the best parameter combination based on the scores
     best_idx = np.argmin(scores)
     best_params = parameters[best_idx]
-    best_score = scores[best_idx]
 
-    print('Best parameters:', best_params)
-    print('Best score:', best_score)
+    print('Best parameters:', best_params,'\n')
 
-    # Train the model with the best parameter combination
     best_model = FMScoreWrapper(**best_params)
     best_model.fit(train)
-
-    # Convert test data to sparse matrix
     test_coo = coo_matrix(test)
-
-    # Get the true ratings from the test set
     true_ratings = test_coo.data
-
-    # Get the corresponding predicted ratings
     pred_ratings = best_model.predict(test_coo.row, test_coo.col)
-
-    # Clip predictions between 0 and 1
     pred_ratings = np.clip(pred_ratings, 0, 1)
 
     # Compute MAE in train and test sets
@@ -171,9 +152,9 @@ def model_simulation(file_path, selected_date, category):
 
     print('Train Set - MAE:', train_mae)
     print('Test Set - MAE:', test_mae)
-    print("User feature matrix dimensions:", matrix.shape)
+    print("User feature matrix dimensions:", matrix.shape,'\n')
 
-    # Get all combinations of stores and products
+    # Get all combinations of stores and products scores
     if category == 'cold_start':
         pred, deja = [], []
         for store in y.index.tolist():
@@ -184,20 +165,17 @@ def model_simulation(file_path, selected_date, category):
                     col = np.array(
                         [df_unique_products[df_unique_products['Material_ID'] == product].index[0]])
 
-                    # Use the predict method of the LightFM model to get the predicted score
-                    score = model.predict(row, col)
-                    # Transform predicted scores using sigmoid function
+                    # Predict and transform predictions
+                    score = model.predict(row, col)                    
                     score = expit(score)
-
-                    # Clip predictions to (0, 1) range
                     score = np.clip(score, 0, 1)
                     pred += [score]
                     deja += [y.loc[store, product]]
-        print(len(pred), len(deja))
+
         rmse_sim = np.sqrt(mean_squared_error(pred, deja))
         mae_sim = mean_absolute_error(pred, deja)
-        print('RMSE cold{}: '.format(selected_date), rmse_sim)
-        print('MAE cold: ', mae_sim)
+        print(f'RMSE Cold Start month {selected_date}: ', rmse_sim)
+        print(f'MAE Cold Start month {selected_date}: ', mae_sim)
     elif category == 'partially_new':
         pred, deja = [], []
         for store in list(not_null_legit.keys()):
@@ -208,17 +186,17 @@ def model_simulation(file_path, selected_date, category):
                 col = np.array(
                     [df_unique_products[df_unique_products['Material_ID'] == product].index[0]])
 
-                # Use the predict method of the LightFM model to get the predicted score
+                # Predict and transform prediction
                 score = model.predict(row, col)
-                # Transform predicted scores using sigmoid function
                 score = expit(score)
-
-                # Clip predictions to (0, 1) range
                 score = np.clip(score, 0, 1)
                 pred += [score]
                 deja += [sc]
-        print(len(pred), len(deja))
+
         rmse_sim = np.sqrt(mean_squared_error(pred, deja))
         mae_sim = mean_absolute_error(pred, deja)
-        print('RMSE pn{}: '.format(date), rmse_sim)
-        print('MAE pn: ', mae_sim)
+        print(f'RMSE Partially New month {selected_date}: ', rmse_sim)
+        print(f'MAE Partially New month {selected_date}: ', mae_sim)
+
+# To obtain RMSE and MAE for month 2 for a cold start
+model_simulation('data','2','cold_start')
